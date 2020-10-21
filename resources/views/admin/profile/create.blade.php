ac9324c94e987055027c093f65ce8fc613593157
Mashi <mune.andmore@gmail.com>
2020-10-21 19:04:01 +0000

    PHP/Laravel11 Kadai teishutu

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,17 @@
         <title>Mashi's blog</title>
     </head>
     <body>
+        @extends('layouts/profile')
+        @section('title')
+        @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
         <h1>Mashi's blog create view</h1>
+        
+                    <h2></h2>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
