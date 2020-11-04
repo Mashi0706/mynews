b13563b7da8c9c19003f8743b43f0517b4588202
Mashi <mune.andmore@gmail.com>
2020-11-04 08:29:36 +0000

    Heroku DB設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (\APP::environment('production')) {
+        if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
     }
